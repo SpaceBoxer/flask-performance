37a3c8d7457cb8f6f6b24e937a6fb0442097e8c9
slyang-git <slyang@aliyun.com>
2019-04-17 10:27:55 +0800

    Add dependencies

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Flask-Performance",
-    version="0.0.1",
+    version="0.0.2",
     author="Spacebox(盒子空间) Team",
     author_email="spacebox@spacebox.fun",
-    description="A small example package",
+    description="A Flask extension to monitoring every REAL api request and collect it's performance in REAL-time.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/SpaceBoxer/flask-performance",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'flask',
+        'requests'
+    ]
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
