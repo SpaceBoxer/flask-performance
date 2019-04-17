0a40370e2000cc1e900b9fb8312ce491e9380382
slyang-git <slyang@aliyun.com>
2019-04-17 11:22:27 +0800

    Fix bug

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     install_requires=[
         'flask',
         'requests'
-    ]
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
